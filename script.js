e514b82020bcb424a0cc74c733e226e8888f8394
unknown <halderurmi@gmail.com>
2026-02-23 12:02:18 +0600

    filter-btns events added

--- script.js
+++ script.js
@@ -15,3 +15,35 @@ const rejectFilterBtn = document.getElementById("reject-filter-btn");
 let currentFilter = "all" ;
 
 let jobs = [];
+
+const cards = document.querySelectorAll(".main-cards");
+
+let idCounter = 1 ;
+
+for(let card of cards){
+    jobs.push({
+        id: idCounter,
+        element: card,
+        status: "all"
+    });
+
+    idCounter = card.dataset.id ;
+
+    idCounter++ ;
+}
+updateUI();
+// filter buttons events
+allFilterBtn.addEventListener('click', function(){
+    currentFilter = "all" ;
+    updateUI();
+});
+
+interviewFilterBtn.addEventListener('click', function(){
+    currentFilter = "interview" ;
+    updateUI();
+});
+
+rejectFilterBtn.addEventListener('click', function(){
+    currentFilter = "rejected" ;
+    updateUI();
+});
